81c0abf2dff6166779ea1517f0ee14b2496363fb
ivandelaparte <ivandelaparte@gmail.com>
2023-10-19 16:28:59 -0300

    Implementacón de consumo de API propia.

---
+++ src/scripts/catalog.js
@@ -0,0 +1,52 @@
+const catalogContent = document.getElementById("catalogContent");
+const seeCart = document.getElementById("seeCart");
+
+let cart = []
+
+function getProducts(done) {
+    const results = fetch('https://productos-tienda-b9e14-default-rtdb.firebaseio.com/productos.json');
+
+    results
+        .then(res=>res.json())
+        .then(json=>done(json));
+}
+
+getProducts(json => {
+    for (let i = 1; i < json.length; i++) {
+        let content = document.createElement("div");
+        content.className = "product-card";
+        content.innerHTML = `
+            <div class="product-card animate__animated animate__fadeIn">
+                <figure class="product-figure">
+                    <img loading="lazy" class="product-img" src=${json[i].image} alt=${json[i].description}>
+                </figure>
+                <div class="product-info">
+                    <h3 class="product-name text-kanit">${json[i].name}</h3>
+                    <p class="product-price text-roboto">${"$"+json[i].price}</p>
+                </div>
+            </div>
+        `;
+    
+        catalogContent.append(content);
+    
+        let buttonAddToCart = document.createElement("button");
+        buttonAddToCart.innerText = "Agregar al carrito";
+        buttonAddToCart.className = "add-to-cart-btn text-roboto";
+
+        content.append(buttonAddToCart);
+
+        buttonAddToCart.addEventListener("click", () => {
+            cart.push({
+                id: json[i].id,
+                name: json[i].name,
+                value: json[i].value,
+                image: json[i].image
+        });
+    });
+    }
+    
+})
+
+seeCart.addEventListener("click", () => {
+    sessionStorage.setItem("cart",JSON.stringify(cart));
+});
